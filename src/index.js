99e15804f96897bb12847744984a328d09a8707d
Jon Walz <jon.walz.music@gmail.com>
2018-12-04 20:53:34 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*
+   Form info can be found here:
+   https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
+   
+   
+*/
+
+/*
+  First, we need to add an event listener to the form itself.
+*/
+
+function submitForm(a, b) {
+    console.log("A: ", a);
+    console.log("B: ", b);
+}
+
+console.log("Loaded");
